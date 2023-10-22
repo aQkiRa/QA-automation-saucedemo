735053623e0c8a907b0e453821d0cd8ad921fc98
Reza <rezaadhityajanuar@gmail.com>
2023-10-22 21:46:55 +0700

    initial push

---
+++ src/main/java/firstProgram/PercabanganProgram.java
@@ -0,0 +1,19 @@
+//package firstProgram;
+//import java.util.Scanner;
+//public class PercabanganProgram {
+//    public static void main(Sting[] args){
+//        Scanner scanner = new Scanner(System.id);
+//        String password = prompt("Enter Password");
+//        String validPassword = "Password";
+//
+//        if (password.equals(validPassword)) {
+//            System.out.println();
+//        }
+//    }
+//
+//    public static String prompt(String s) {
+//        Scanner scanner =  new Scanner(System.id);
+//        System.out.println(message);
+//        return scanner.nextLine();
+//    }
+//}
